06f9c6e9588abe8f412535d2c38bf343c13cecaf
Benlee1000 <benny01lee@gmail.com>
2022-04-18 10:11:03 -0700

    Tried to handle singal SIGCHLD

--- HW1/timeout.c
+++ HW1/timeout.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <time.h>
+
+// void kill_parent(int signum) {
+//     printf("Child finished first\n");
+//     kill(getppid(), SIGTERM);
+// }
+
 
 int main(int argc, char *argv[]) {
 
     char *newenviron[] = { NULL };
+    struct sigaction info;
+    // int start = 0;
+    // int trigger = atoi(argv[1]);
 
     // printf("%s %s %s", argv[0], argv[1], argv[2]);
     if (*argv[1] == '-') {
@@ -32,14 +42,17 @@ int main(int argc, char *argv[]) {
             break;
         case 0:
             execve(argv[2], new_arg, newenviron);
+            wait();
+            printf("Child Finished First");
+            exit(0);
             break;
         default:
             sleep(atoi(argv[1]));
-            // if(sigaction(SIGCHILD) == "0") {
-            //     printf("done");
-            //     exit(0);
-            // }
+            // signal(SIGCHLD, kill_parent);
+            printf("Parent finished first");
             kill(rc, SIGTERM);
             break;
     }
 }
+
+
