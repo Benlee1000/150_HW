ffd5e33b3ea7a5b6970e0446adb5cbe5f7ddb4db
Benlee1000 <benny01lee@gmail.com>
2022-04-24 18:42:54 -0700

    Fixed 1 and 2 for submission.

--- HW1/timeout.c
+++ HW1/timeout.c
@@ -5,34 +5,51 @@
 #include <time.h>
 
 
-void sigchld_handler(pid_t signum);
+void sigchld_handler(int signum);
+int kill(int fork, int);
+int isdigit(int arg);
+char *strcpy(char *dest, const char *src);
 
 int main(int argc, char *argv[]) {
 
-    char *newenviron[] = { NULL };
-    int i;
+    char* newenviron[] = { NULL };
+    char** new_arg = malloc((argc-2)*sizeof(char));
+    int rc;
+    int i = 0;
 
-    // printf("%s %s %s", argv[0], argv[1], argv[2]);
-    if (argc <= 2) {
-      perror("command and/or seconds are missing\n");
-        exit(1);
+    /* printf("%s %s %s", argv[0], argv[1], argv[2]); */
+    if(argc < 3){
+        printf("Usage: timeout sec command [args ...]\n");
+        exit(0);
     }
 
-    else if (atoi(argv[1]) < 0) {
-      perror("arg is negative\n");
-        exit(1);
+    if(atoi(argv[1]) < 0){
+        printf("%d is not a positive integer\n", atoi(argv[1]));
+        exit(0);
     }
     
-    pid_t rc;
-    char *new_arg[argc];
-
-    for (i = 2; i < argc; i++) {
-        new_arg[i-2] = argv[i];
+    if(isdigit(*argv[1]) == 0){ 
+        printf("%s is not a positive integer\n", argv[1]);
+        exit(0);
     }
+
+    if(access(argv[2],F_OK)!=0){
+        perror("execve");
+        exit(0);
+    }
+
+    
+    
+    /* Parse correct arguments for execve */
+    for (; i < argc-2; i++) {
+        new_arg[i] = (char*)malloc(sizeof(argv[i+2]));
+        strcpy(new_arg[i],argv[i+2]);
+    }
+    new_arg[argc-2] = '\0';
 
     switch(rc = fork()) {
         case -1:
-            perror("fork() failed");
+            perror("fork");
             exit(1);
             break;
         case 0:
@@ -41,13 +58,14 @@ int main(int argc, char *argv[]) {
         default:
             signal(SIGCHLD, sigchld_handler);
             sleep(atoi(argv[1]));
-            // printf("Parent finished first");
+            /* printf("Parent finished first"); */
             kill(rc, SIGTERM);
             break;
     }
+    return 0;
 }
 
-void sigchld_handler(pid_t signum) {
-    // printf("Child finished first\n");
-    kill(signum, SIGTERM);
+void sigchld_handler(int signum) {
+    /* printf("Child finished first\n"); */
+    /* kill(signum, SIGTERM); */
 }
